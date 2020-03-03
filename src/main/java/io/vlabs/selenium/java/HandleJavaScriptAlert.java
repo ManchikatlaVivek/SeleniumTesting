81d9520b3c17718332630422c9321a67d2ed2fc0
Vivek Manchikatla <vivekmanchikatla1996@gmail.com>
2020-03-03 22:08:01 +0530

    Create Java Scirpt Alert Handle Test Case

---
+++ src/main/java/io/vlabs/selenium/java/HandleJavaScriptAlert.java
@@ -0,0 +1,29 @@
+package io.vlabs.selenium.java;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleJavaScriptAlert {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Eclipse Installation\\workspace-tomcat\\testng-basics\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		
+		// click on login button without filling any data.
+		
+		driver.findElement(By.name("proceed")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		
+		System.out.println(alert.getText());
+		
+		alert.accept();
+	}
+
+}
