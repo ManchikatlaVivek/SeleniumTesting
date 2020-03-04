51731e988d9ea160fa953d4386105a8a8b130d9f
Vivek Manchikatla <vivekmanchikatla1996@gmail.com>
2020-03-04 18:09:28 +0530

    Handle File Upload Test Case

---
+++ src/main/java/io/vlabs/selenium/java/HandleFileUpload.java
@@ -0,0 +1,24 @@
+package io.vlabs.selenium.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleFileUpload {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Eclipse Installation\\workspace-tomcat\\testng-basics\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://html.com/input-type-file/");
+		
+		driver.findElement(By.id("fileupload")).sendKeys("C:\\Eclipse Installation\\workspace-tomcat\\SeleniumTesting\\index.html");
+		
+		
+		
+	}
+
+}
