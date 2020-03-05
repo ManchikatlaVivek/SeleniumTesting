72902a3a21243098750f76ada2261a52b501ada6
Vivek Manchikatla <vivekmanchikatla1996@gmail.com>
2020-03-05 10:00:14 +0530

    Create DragAndDropTestCase.java

---
+++ src/main/java/io/vlabs/selenium/java/DragAndDropTestCase.java
@@ -0,0 +1,33 @@
+package io.vlabs.selenium.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDropTestCase {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Eclipse Installation\\workspace-tomcat\\testng-basics\\drivers\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+		driver.get("https://jqueryui.com/droppable/");
+
+	// the below two lines are working, choose any one of them for testing;
+		driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
+//		driver.switchTo().frame(0);
+
+		Thread.sleep(3000);
+
+		Actions action = new Actions(driver);
+
+		action.clickAndHold(driver.findElement(By.xpath("//*[@id=\"draggable\"]")))
+				.moveToElement(driver.findElement(By.xpath("//*[@id=\"droppable\"]"))).release().build().perform();
+
+	}
+
+}
