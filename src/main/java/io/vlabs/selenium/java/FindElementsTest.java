356aee96c4de4babae98a5095fb1cea7aae493ad
Vivek Manchikatla <vivekmanchikatla1996@gmail.com>
2020-03-06 10:09:22 +0530

    Create FindElementsTest.java

---
+++ src/main/java/io/vlabs/selenium/java/FindElementsTest.java
@@ -0,0 +1,37 @@
+package io.vlabs.selenium.java;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementsTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Eclipse Installation\\workspace-tomcat\\testng-basics\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.amazon.in/");
+		
+		List<WebElement> list = driver.findElements(By.tagName("a"));
+		
+		System.out.println(list.size());
+		
+		int lenList = list.size();
+		for(int i=0;i<lenList;i++) {
+			String listText = list.get(i).getText();
+			System.out.println(listText);
+		}
+
+	}
+
+}
